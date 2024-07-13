170c8f79a38380984d2fcfd7bf8f17544e1919ac
Vxrcel <skyeparagon@gmail.com>
2024-07-13 22:06:14 +0100

    Update page.tsx

--- app/(processed)/thank-you/page.tsx
+++ app/(processed)/thank-you/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import BlurImage from "@/components/miscellaneous/blur-image";
 
 type Props = {};
 
@@ -15,6 +16,15 @@ const ThankYou = (props: Props) => {
 	return (
 		<div className="w-full min-h-screen flex justify-center">
 			<div className="flex flex-col gap-y-4 items-center">
+				<BlurImage
+					src="https://cdni.iconscout.com/illustration/premium/thumb/order-confirmed-5115435-4273317.png"
+					width={290}
+					height={314}
+					alt="product"
+					className="max-w-[90%] md:max-w-[600px] w-full "
+					draggable={false}
+					quality={100}
+				/>
 				<h1 className="sm:text-2xl">
 					Thank You {name} for buying {items_length} items from our store
 				</h1>
